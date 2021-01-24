130fd2d711cf3bc5f81fbc7e09f488d6d8a1f9da
TheGreatStorm <thegreatstormx@gmail.com>
2021-01-24 17:20:06 -0600

    Testing

--- rustserver.py
+++ rustserver.py
@@ -178,7 +178,6 @@ if args.command:
         print("--------------------------------------------------------")
         if game_installed != 'unset':
             game_config = get_game_config(prefix_dir, game_config, current_game)
-            print(game_config)
             result = json.dumps(game_config)
             print(result)
             server_info = {}
