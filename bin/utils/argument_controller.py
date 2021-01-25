eca79acd6da0f5218baa355999576dee068008fc
TheGreatStorm <thegreatstormx@gmail.com>
2021-01-24 19:02:32 -0600

    Testing

--- bin/utils/argument_controller.py
+++ bin/utils/argument_controller.py
@@ -11,10 +11,10 @@ def argument_controller():
     parser.add_argument('--restart', help='Restart Rust Server', required=False, action='store_true')
     parser.add_argument('--update', help='Update Rust Server', required=False, action='store_true')
     parser.add_argument('--install_mod', help='Install Oxide', required=False, action='store_true')
-    parser.add_argument('--download_plugin', help='DownloadPlugin', required=False)
-    parser.add_argument('--command', help='Run Rcon Command', required=False)
     parser.add_argument('--partial_wipe', help='Clean up Maps', required=False, action='store_true')
     parser.add_argument('--full_wipe', help='Clean up everything!', required=False, action='store_true')
     parser.add_argument('--clean', help='Destroys server folder', required=False, action='store_true')
+    parser.add_argument('--download_plugin', help='Download Plugin', required=False)
+    parser.add_argument('--command', help='Run Rcon Command', required=False)
     args = parser.parse_args()
     return args
