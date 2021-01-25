6f314da82184a5ece73e9dac1bcf61864cf76ed1
TheGreatStorm <thegreatstormx@gmail.com>
2021-01-24 19:22:49 -0600

    Fix?

--- rustserver.py
+++ rustserver.py
@@ -208,7 +208,7 @@ if args.download_plugin:
             # print(server_info)
             playbook_name = "plugin_install.yml"
             playbook = os.path.abspath(os.path.join(prefix_dir, "playbooks/{}/{}".format(current_game, playbook_name)))
-            game_config = "{'plugin_name': ' " + plugin_name + "'}"
+            game_config = '{"plugin_name": "' + plugin_name + '"}'
             print(game_config)
             run_playbook(playbook, game_config)
         else:
